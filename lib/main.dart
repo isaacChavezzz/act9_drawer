3fcb58b4fd009e2b1c33a3ccff2406a395caab34
isaacChavezzz <143744084+isaacChavezzz@users.noreply.github.com>
2024-02-26 08:48:26 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,133 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiDrawerChavez());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiDrawerChavez extends StatelessWidget {
+  const MiDrawerChavez({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xff66640b),
+            title: const Text("Mi Drawer Chavez"),
+          ),
+          backgroundColor: const Color(0xffdcbb55),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text("Isaac Abel Chavez Nolasco"),
+                  accountEmail: Text("a.21308051280336@cbtis128.edu.mx"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://raw.githubusercontent.com/isaacChavezzz/img_IOS/main/xd.png"),
+                  ),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        "https://raw.githubusercontent.com/isaacChavezzz/img_IOS/main/nose.jpg",
+                      ),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  otherAccountsPictures: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/isaacChavezzz/img_IOS/main/telcel.jpg"),
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/isaacChavezzz/img_IOS/main/mexico.png"),
+                    ),
+                  ],
+                ),
+                ListTile(
+                  leading: const Icon(Icons.assignment_ind_outlined,
+                      color: Colors.black, size: 35),
+                  title: const Text(
+                    "Cuenta",
+                    textScaleFactor: 1.7,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading:
+                      const Icon(Icons.sim_card, color: Colors.black, size: 35),
+                  title: const Text(
+                    "chips",
+                    textScaleFactor: 1.7,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.smartphone,
+                      color: Colors.black, size: 35),
+                  title: const Text(
+                    "Telefonos",
+                    textScaleFactor: 1.7,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.signal_wifi_0_bar,
+                      color: Colors.black, size: 35),
+                  title: const Text(
+                    "Recargas",
+                    textScaleFactor: 1.7,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading:
+                      const Icon(Icons.five_g, color: Colors.black, size: 35),
+                  title: const Text(
+                    "Paquetes",
+                    textScaleFactor: 1.7,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.headphones,
+                      color: Colors.black, size: 35),
+                  title: const Text(
+                    "Accesorios",
+                    textScaleFactor: 1.7,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading:
+                      const Icon(Icons.login, color: Colors.black, size: 35),
+                  title: const Text(
+                    "Cerrar Sesion",
+                    textScaleFactor: 1.7,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+          ),
+        )); //material
+  } //widget
+} //MiDrawerChavez
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
